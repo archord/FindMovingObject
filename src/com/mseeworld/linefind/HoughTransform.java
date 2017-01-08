ca1232839754578a388261325c8b990865069a72
archord <xyag.902@163.com>
2017-01-08 23:41:05 +0800

    功能增加：1，删除部分调试信息。

--- src/com/mseeworld/linefind/HoughTransform.java
+++ src/com/mseeworld/linefind/HoughTransform.java
@@ -168,11 +168,6 @@ public class HoughTransform {
     boolean findLine = false;
     int i = 0;
     for (LineObject tline : this.mvObjs) {
-      
-      if(ot1.getFfNumber()==2473){
-        System.out.println("here");
-      }
-
       if (!tline.isEndLine(ot1.getFfNumber()- this.maxHoughFrameNunmber + 1)) {
         if (tline.isOnLine(ot1)) {
           tline.addPoint(numOT1s - 1, ot1.getFfNumber(), ot1.getX(), ot1.getY(), ot1.getDateUt(), ot1.getOorId());
